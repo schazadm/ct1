e2081539b6b863e4c7aa07c2531b14fb4d5651e5
Schazad Mohammad <mohamsch@students.zhaw.ch>
2022-11-17 23:30:31 +0100

    lab 07 done

--- lab07/app/struct_code.s
+++ lab07/app/struct_code.s
@@ -60,6 +60,7 @@ DISPLAY_BIT               DCB        "Bit "
 DISPLAY_2_BIT             DCB        "2"
 DISPLAY_4_BIT             DCB        "4"
 DISPLAY_8_BIT             DCB        "8"
+DISPLAY_ZERO_COUNTER      DCB        " Zero(s)"
         ALIGN
 
 ; ------------------------------------------------------------------
@@ -79,133 +80,171 @@ main    PROC
         BL         clear_lcd
 
 main_loop
-; STUDENTS: To be programmed
+        BL adc_get_value ; read adc_value (32bit) into r0
 
-		; read adc_value (32bit) into r0
-        BL adc_get_value
-
-        PUSH    {r0, r1, r2}
-        LDR     r1, =ADDR_BUTTONS ; load base address of keys
-        LDR     r2, =MASK_KEY_T0 ; load key mask T0
-        LDRB    r0, [r1] ; load key values
-        ; Z flag is set to 1 when the result of the operation is zero, cleared to 0 otherwise.
-		TST     r0, r2 ; check, if key T0 is pressed.
-        POP     {r0, r1, r2}
-        BNE     else_0 ; check if Z flag is not set (Z==0)
+        PUSH {R0, R1, R2}
+        LDR R1, =ADDR_BUTTONS ; load base address of keys
+        LDR R2, =MASK_KEY_T0 ; load key mask T0
+        LDRB R0, [R1] ; load key values
+        
+		TST R0, R2 ; check, if key T0 is pressed.
+        POP {R0, R1, R2}
+        
+		BNE else_0 ; check if Z flag is not set (Z==0)
 		
 then_0
-        ; set LCD color
-        LDR     r7, =DISPLAY_COLOUR_GREEN
-        BL      set_lcd_color
+        LDR R7, =DISPLAY_COLOUR_GREEN
+        BL set_lcd_color
 
-        ; output adv_value to 7SEG
-        LDR     r7, =ADDR_7_SEG_BIN_DS3_0
-        STRH    r0, [r7, #0]
+        LDR R7, =ADDR_7_SEG_BIN_DS3_0
+        STRH R0, [R7, #0]
+		
+		; show LED0 first
+		LDR R4, =ADDR_LED_31_0
+		LDR R7, =0x00000001
+		STR R7, [R4]
+		
+		; if value is FF, add one otherwise the loop stops to early for some reason
+		MOVS R7, #0xff
+		CMP R0, R7
+		BL value_is_ff
 
-        B end_0
+		; FIXME: maybe board at home is not functioning correctly,
+		; have to -1 because ADC=0 is 1 in register R0
+		SUBS R0, #1
+
+		; calc value to build LED bar
+		LSRS R0, #3 ; division by 8 to scale the ADC-value from 8-bit down to 5-bit
+		MOVS R2, #1 ; prepare for the LED bar -> set to 1 because LED0 is already active
+
+magnitude_bar_loop
+		CMP R0, #0 ;check if R0 is empty
+		BEQ end_0
+		
+		LSLS R2, #1
+		ADDS R2, #1
+		B display_led_bar
+		
+display_led_bar
+		LDR R4, =ADDR_LED_31_0
+		STR R2, [R4]
+		SUBS R0, #1
+		B magnitude_bar_loop
+		
+value_is_ff
+		ADDS R0, #1
+		BX LR
+
+end_0
+        B main_loop
 
 else_0
         ; read dip switch S7 S0 to r1
-        LDR     r7, =ADDR_DIP_SWITCH_7_0
-        LDRB    r1, [r7]
-        SUBS    r1, r1, r0
-
-        BLT     else_1
-
+        LDR R7, =ADDR_DIP_SWITCH_7_0
+        LDRB R1, [R7]
+		; diff = dip_switches - ADC_value
+        SUBS R1, R1, R0
+		
+        BLT else_1 ; diff < 0
+		
 then_1
-        ; set LCD color
-        LDR     r7, =DISPLAY_COLOUR_BLUE
-        BL      set_lcd_color
+        LDR R7, =DISPLAY_COLOUR_BLUE
+        BL set_lcd_color
+		
+        CMP R1, #4 ; r1 - 4
+        BLT block_2_0 ; diff < 4
 
-        CMP r1, #4 ; r1 - 4
-        BLT block_2_0 ; < 4
-
-        CMP r1, #16 ; r1 - 16
-        BLT block_2_1 ; < 16
+        CMP R1, #16 ; r1 - 16
+		BLT block_2_1 ; diff < 16
 
         B default_2
 
 ; display 2 Bit on LCD
 block_2_0
-        LDR        r7, =ADDR_LCD_ASCII
-        LDR        r6, =DISPLAY_2_BIT
-        LDRB       r6, [r6]
-        STRB       r6, [r7]
-        B end_1
+        LDR R7, =ADDR_LCD_ASCII
+        LDR R6, =DISPLAY_2_BIT
+        LDRB R6, [R6]
+        STRB R6, [R7]
+		
+        B write_bit
 
 ; display 4 Bit on LCD
 block_2_1
-        LDR        r7, =ADDR_LCD_ASCII
-        LDR        r6, =DISPLAY_4_BIT
-        LDRB       r6, [r6]
-        STRB       r6, [r7]
+        LDR R7, =ADDR_LCD_ASCII
+        LDR R6, =DISPLAY_4_BIT
+        LDRB R6, [R6]
+        STRB R6, [R7]
 
-        B end_1
+        B write_bit
 
 ; display 8 Bit on LCD
 default_2
-        LDR        r7, =ADDR_LCD_ASCII
-        LDR        r6, =DISPLAY_8_BIT
-        LDRB       r6, [r6]
-        STRB       r6, [r7]
-
-        B end_1
+        LDR R7, =ADDR_LCD_ASCII
+        LDR R6, =DISPLAY_8_BIT
+        LDRB R6, [R6]
+        STRB R6, [R7]
+		
+write_bit
+		BL write_bit_ascii
+		B end_1
 
 else_1
-        ; set LCD color
-        LDR     r7, =DISPLAY_COLOUR_RED
-        BL      set_lcd_color
+        LDR R7, =DISPLAY_COLOUR_RED
+        BL set_lcd_color
 
-        ; MOVS r1, #0xff ; todo remove me
+block_3_0
+		PUSH {R1}
+		MOVS R7, #0 ; zero counter
+		MOVS R6, #0 ; iteration counter -> i
+		MOVS R5, #32 ; max iteration n
+		LDR R4, =0xFFFFFFFE ; bitmask to only extract the LSB
 
-        ; count ones in binary (r1), save in r5
-        MOVS r7, r1
-        MOVS r6, #0
-        MOVS r5, #0
-rep     LSRS r7, r7, #1
-        ADCS r5, r5, r6 ; add r5 + carry bit.
-back    CMP r7, #0 ; check if we are done (if r7 == 0)
-        BNE rep
-
-        MOVS r7, #32
-        SUBS r5, r7, r5
-
-        ; todo write number to lcd
-        LDR       r7, =ADDR_LCD_ASCII_2ND_LINE
-        LDR       r5, [r5]
-        STR       r5, [r7]
+block_3_1
+		CMP R6, R5 ; i - 32
+		BEQ block_3_2 ; i == 0 or i < 32
+		
+		ADDS R6, #1 ; i++
+		LSRS R1, #1
+		MOVS R2, R1
+		BICS R2, R4
+		CMP R2, #0 ; r2 - 0
+		BNE block_3_1 ; bit != 0
+		
+		ADDS R7, #1 ; zero counter++
+		B block_3_1
+		
+block_3_2
+		LDR R3, =ADDR_LCD_ASCII
+		LDR R6, =ASCII_DIGIT_OFFSET
+		ADDS R3, R3, R6
+		STRB R7, [R3]
+		POP {R1}
 
 end_1
-        BL write_bit_ascii
-
-        ; output diff to 7SEG
-        LDR     r7, =ADDR_7_SEG_BIN_DS3_0
-        STRH    r1, [r7, #0]
-
-end_0
-
-; END: To be programmed
-        B          main_loop
+        LDR     R7, =ADDR_7_SEG_BIN_DS3_0
+        STRH    R1, [R7, #0]
+		
+		B main_loop
 
 set_lcd_color ; to r7
-        PUSH    {r0, r1, r2}
-        LDR     r0, =ADDR_LCD_COLOUR
+        PUSH    {R0, R1, R2}
+        LDR     R0, =ADDR_LCD_COLOUR
 
-        LDR     r1, =LCD_BACKLIGHT_OFF
+        LDR     R1, =LCD_BACKLIGHT_OFF
 
-        LDR     r2, =DISPLAY_COLOUR_RED
-        STRH    r1, [r0, r2]
+        LDR     R2, =DISPLAY_COLOUR_RED
+        STRH    R1, [R0, R2]
 
-        LDR     r2, =DISPLAY_COLOUR_GREEN
-        STRH    r1, [r0, r2]
+        LDR     R2, =DISPLAY_COLOUR_GREEN
+        STRH    R1, [R0, R2]
 
-        LDR     r2, =DISPLAY_COLOUR_BLUE
-        STRH    r1, [r0, r2]
+        LDR     R2, =DISPLAY_COLOUR_BLUE
+        STRH    R1, [R0, R2]
 
-        LDR     r1, =LCD_BACKLIGHT_FULL
-        STRH    r1, [r0, r7]
+        LDR     R1, =LCD_BACKLIGHT_FULL
+        STRH    R1, [R0, R7]
 		
-        POP     {r0, r1, r2}
+        POP     {R0, R1, R2}
         BX      LR
         ALIGN		
 
